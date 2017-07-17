b93cd6ddc9df85e4f919f9575a4bf90e6b96c9c7
Chris Alderson <chrisalderson@ptotonmail.com>
2017-07-17 23:42:19 +0000

    move examples to standard

--- examples/example.js
+++ examples/example.js
@@ -1,35 +1,35 @@
-'use strict';
+'use strict'
 
-const YTS = require('../yts-api-pt');
+const YTS = require('../yts-api-pt')
 
 const yts = new YTS({
   debug: false
-});
+})
 
 yts.getMovies({
   limit: 20,
   page: 1,
   quality: 'All',
-  minimum_rating: 0,
-  query_term: 'inception',
+  minimumRating: 0,
+  queryTerm: 'inception',
   genre: 'action',
-  sort_by: 'date_added',
-  order_by: 'desc',
-  with_rt_ratings: true
+  sortBy: 'date_added',
+  orderBy: 'desc',
+  withRtRatings: true
 }).then(res => console.log(res))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
 
 yts.getMovie({
-  movie_id: 15,
-  with_images: true,
-  with_cast: true
+  movieId: 15,
+  withImages: true,
+  withCast: true
 }).then(res => console.log(res))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
 
 yts.getSuggestions(15)
   .then(res => console.log(res))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
 
 yts.getParentalGuides(15)
   .then(res => console.log(res))
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
