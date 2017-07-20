1e22241a9758aca3e0244d634a52add8d7504566
Chris Alderson <chrisalderson@ptotonmail.com>
2017-07-20 22:17:48 +0000

    refine example

--- examples/example.js
+++ examples/example.js
@@ -1,11 +1,15 @@
 'use strict'
 
-const YTS = require('../yts-api-pt')
+// Import the necessary modules.
+const YtsApi = require('../yts-api-pt')
 
-const yts = new YTS({
-  debug: false
-})
+// Create a new instance of the module.
+const yts = new YtsApi()
 
+// Define a variable `movieId` for the example.
+let movieId
+
+// Get a list of movies.
 yts.getMovies({
   limit: 20,
   page: 1,
@@ -16,20 +20,27 @@ yts.getMovies({
   sortBy: 'date_added',
   orderBy: 'desc',
   withRtRatings: true
+}).then(res => {
+  console.log(res)
+
+  // Set the moveId.
+  movieId = res.data.movies[0].id
+
+  // Get a single movie.
+  return yts.getMovie({
+    movieId,
+    withImages: true,
+    withCast: true
+  })
+}).then(res => {
+  console.log(res)
+
+  // Get the suggestions of a movie.
+  return yts.getSuggestions(movieId)
+}).then(res => {
+  console.log(res)
+
+  // Get the parental guides of a movie.
+  return yts.getParentalGuides(movieId)
 }).then(res => console.log(res))
-  .catch(err => console.error(err))
-
-yts.getMovie({
-  movieId: 15,
-  withImages: true,
-  withCast: true
-}).then(res => console.log(res))
-  .catch(err => console.error(err))
-
-yts.getSuggestions(15)
-  .then(res => console.log(res))
-  .catch(err => console.error(err))
-
-yts.getParentalGuides(15)
-  .then(res => console.log(res))
   .catch(err => console.error(err))
